b54bb01c09f1ed1246b5f513b1b289e1b9543548
Ars55555 <108430044+Ars55555@users.noreply.github.com>
2024-08-02 03:10:46 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const navItems = [
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
       titleClassName: "justify-end",
-      img: "/b1.svg",
+      img: "./b1.svg",
       spareImg: "",
     },
     {
@@ -43,8 +43,8 @@ export const navItems = [
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
       titleClassName: "justify-start",
-      img: "/grid.svg",
-      spareImg: "/b4.svg",
+      img: "./grid.svg",
+      spareImg: "./b4.svg",
     },
   
     {
@@ -54,8 +54,8 @@ export const navItems = [
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
-      img: "/b5.svg",
-      spareImg: "/grid.svg",
+      img: "./b5.svg",
+      spareImg: "./grid.svg",
     },
     {
       id: 6,
@@ -74,8 +74,8 @@ export const navItems = [
       id: 1,
       title: "Peak Market",
       des: "This web development project realted to e-commerce domain where user buy products at discounted rate.",
-      img: "/p1.svg",
-      iconLists: ["/nodejs.svg", "/react.svg", "/mongodb.svg", "/javascript.svg", "/bootstrap.svg"],
+      img: "./p1.svg",
+      iconLists: ["./nodejs.svg", "./react.svg", "./mongodb.svg", "./javascript.svg", "./bootstrap.svg"],
       link: "https://peakmarket.onrender.com/",
     },
     {
