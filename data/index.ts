be46b888279559ff6c615d52b822388ce4bee1f8
Ars55555 <108430044+Ars55555@users.noreply.github.com>
2024-08-02 01:57:08 +0530

    akshay portfolio

---
+++ data/index.ts
@@ -0,0 +1,233 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Transforming Concepts into Code.",
+      description: "",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to hire me?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Peak Market",
+      des: "This web development project realted to e-commerce domain where user buy products at discounted rate.",
+      img: "/p1.svg",
+      iconLists: ["/nodejs.svg", "/react.svg", "/mongodb.svg", "/javascript.svg", "/bootstrap.svg"],
+      link: "https://peakmarket.onrender.com/",
+    },
+    {
+      id: 2,
+      title: "Meme Generator",
+      des: "The Meme Generator Project is a creative and entertaining web application designed to empower users with the ability to effortlessly create and share humorous memes",
+      img: "/p2.svg",
+      iconLists: ["/nodejs.svg", "/react.svg", "/mongodb.svg", "/javascript.svg", "/bootstrap.svg"],
+      link: "https://meme-inventor.netlify.app/",
+    },
+    {
+      id: 3,
+      title: "Pet Adoption",
+      des: "Pet Adoption is a platform where potential pet owners can easily find and adopt animals, offering a variety of pets from shelters and rescues",
+      img: "/p3.svg",
+      iconLists: ["/nodejs.svg", "/react.svg", "/mongodb.svg", "/javascript.svg", "/bootstrap.svg"],
+      link: "",
+    },
+    {
+      id: 4,
+      title: "New Project Coming Soon",
+      des: " Site will be built using the MERN stack ",
+      img: "/p4.svg",
+      iconLists: ["/nodejs.svg", "/react.svg", "/mongodb.svg", "/javascript.svg", "/bootstrap.svg"],
+      link: "",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Akshay was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Akshay's enthusiasm for every facet of development truly stands out.",
+      name: "Bishant Raj",
+      title: "",
+    },
+    {
+      quote:
+        "Akshay transforms ideas into innovative solutions with skill and dedication. Combining technical expertise with creative problem-solving, Akshay is committed to making a positive impact through every line of code and every project developed",
+      name: "Chandresh Sahu",
+      title: "",
+    },
+    {
+      quote:
+        "With a passion for technology and a knack for problem-solving, Akshay brings ideas to life through code. Their dedication and creativity shine in every project, crafting solutions that make a difference in the digital world.",
+      name: "Tushar Bisen",
+      title: "",
+    },
+    {
+      quote:
+        "Akshay excels at turning complex challenges into elegant solutions. With a deep understanding of technology and a flair for innovation, they create impactful and intuitive digital experiences that drive progress and inspire others",
+      name: "Ayush Sahu",
+      title: "",
+    },
+    {
+      quote:
+        "Akshay bridges imagination and technology, transforming visions into functional and beautiful digital solutions. Their expertise and creativity drive the future of development, making every project a testament to their dedication and skill.",
+      name: "Aman Choudhary",
+      title: "",
+    },
+    
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "aws cloud",
+      img: "/awscloud.svg",
+      // nameImg: "/awsname.svg",
+    },
+    {
+      id: 2,
+      name: "java",
+      img: "/java.svg",
+      // nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "react",
+      img: "/react.svg",
+      // nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "node",
+      img: "/nodejs.svg",
+      // nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "mysql",
+      img: "/mysql.svg",
+      // nameImg: "/dockerName.svg",
+    },
+    {
+      id: 6,
+      name: "javascript",
+      img: "/javascript.svg",
+      // nameImg: "/dockerName.svg",
+    },
+    {
+      id: 7,
+      name: "mongodb",
+      img: "/mongodb.svg",
+      // nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Persistent Summer Intern",
+      desc: "Learn the development of a Java Programming and MySql Database.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+      website: "https://drive.google.com/file/d/1Hh9kU0r-MQSWgg18KxY9ZP3xNWRp9QYg/view",
+    },
+    {
+      id: 2,
+      title: "Java Development",
+      desc: "A skilled Java developer in Java Programming , specializing in building robust APIs and scalable systems using Java frameworks like Spring Boot and Hibernate. Proficient in databases and passionate about continuous learning. ",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+      website: "https://drive.google.com/file/d/149C6_qLSCt-H6nWgCRvA8mSrui1HiHeV/view?usp=sharing",
+    },
+    {
+      id: 3,
+      title: "Web Dev Projects",
+      desc: "Led the development of a Web app for a college Projects, from initial concept to deployment on server.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+      website: "",
+    },
+    {
+      id: 4,
+      title: "Web Development",
+      desc: "Developed and maintained user-facing features using modern Web Development technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+      website: "",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/Ars55555" ,
+      
+    },
+    {
+      id: 2,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/akshay-raj-singh-0939451b9/" ,
+    },
+  ];
